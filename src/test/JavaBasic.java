ac329b3fbccdd5a0c1ddee3184d3783d678f8b14
kojima <kmsuser@KMS8103.asynet.intra.anasystems.co.jp>
2018-05-01 13:44:15 +0900

    Slack

--- src/test/JavaBasic.java
+++ src/test/JavaBasic.java
@@ -5,5 +5,6 @@ public class JavaBasic {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("児島だよ！！！！！");
+		System.out.println("Slack通知検証！！！");
 	}
 }
